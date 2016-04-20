839c6b5bc00a59316aa3fceac245aa28a083cfd8
goodwid <goodwid@gmail.com>
2016-04-20 16:22:53 -0700

    Removed commentary, prepped to hand in.

--- scripts/article.js
+++ scripts/article.js
@@ -1,7 +1,5 @@
 (function(module) {
   function Article (opts) {
-    // DONE: Convert property assignment to Functional Programming style. Now, ALL properties
-    // of `opts` will be assigned as properies of the newly created article object.
     Object.keys(opts).forEach(function(e, index, keys) {
       this[e] = opts[e];
     },this);
@@ -19,10 +17,9 @@
     return template(this);
   };
 
-  // COMPLETED: Set up a DB table for articles.
   Article.createTable = function(callback) {
     webDB.execute(
-      'CREATE TABLE articles (id INTEGER PRIMARY KEY, title TEXT, category TEXT, author TEXT, authorUrl TEXT, publishedOn TEXT, body TEXT);', // what SQL command do we run here inside these quotes?
+      'CREATE TABLE articles (id INTEGER PRIMARY KEY, title TEXT, category TEXT, author TEXT, authorUrl TEXT, publishedOn TEXT, body TEXT);',
       function(result) {
         console.log('Successfully set up the articles table.', result);
         if (callback) callback();
@@ -30,10 +27,9 @@
     );
   };
 
-  // COMPLETED: Use correct SQL syntax to delete all records from the articles table.
   Article.truncateTable = function(callback) {
     webDB.execute(
-      'DELETE * FROM articles;', // <----finish the command here, inside the quotes.
+      'DELETE * FROM articles;',
       function(result) {
         console.log('Successfully deleted all records from articles.', result);
         if (callback) callback();
@@ -42,7 +38,6 @@
   };
 
 
-  // COMPLETED: Insert an article instance into the database:
   Article.prototype.insertRecord = function(callback) {
     console.log('insertRecord called');
     webDB.execute(
@@ -59,7 +54,6 @@
     );
   };
 
-  // DONE: Delete an article instance from the database:
   Article.prototype.deleteRecord = function(callback) {
     webDB.execute(
       [
@@ -75,7 +69,6 @@
     );
   };
 
-  // Completed: Update an article instance, overwriting it's properties into the corresponding record in the database:
   Article.prototype.updateRecord = function(callback) {
     webDB.execute(
       [
@@ -91,36 +84,25 @@
     );
   };
 
-  // DONE: Refactor to expect the raw data from the database, rather than localStorage.
   Article.loadAll = function(rows) {
     Article.all = rows.map(function(ele) {
       return new Article(ele);
     });
   };
 
-  // COMPLETED: Refactor this to check if the database holds any records or not. If the DB is empty,
-  // we need to retrieve the JSON and process it.
-  // If the DB has data already, we'll load up the data (sorted!), and then hand off control to the View.
+
   Article.fetchAll = function(next) {
-    webDB.execute('SELECT * FROM articles;', function(rows) { // COMPLETED: fill these quotes to 'select' our table.
+    webDB.execute('SELECT * FROM articles;', function(rows) {
       if (rows.length) {
         Article.loadAll(rows);
         next();
-        // COMPLETED: Now, 1st - instanitate those rows with the .loadAll function,
-        // and 2nd - pass control to the view by calling whichever function argument was passed in to fetchAll.
-
       } else {
         $.getJSON('/data/hackerIpsum.json', function(rawData) {
-          // Cache the json, so we don't need to request it next time:
           rawData.forEach(function(item) {
-            var article = new Article(item); // Instantiate an article based on item from JSON
-            // COMPLETED: Cache the newly-instantiated article in the DB: (what can we call on each 'article'?)
+            var article = new Article(item);
             article.insertRecord();
           });
-          // Now get ALL the records out the DB, with their database IDs:
-          webDB.execute('SELECT * FROM articles;', function(rows) { // COMPLETED: select our now full table
-            // COMPLETED: Now, 1st - instanitate those rows with the .loadAll function,
-            // and 2nd - pass control to the view by calling whichever function argument was passed in to fetchAll.
+          webDB.execute('SELECT * FROM articles;', function(rows) {
             Article.loadAll(rows);
             next();
           });
