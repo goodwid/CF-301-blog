ae1c336130b06914870e3d89af89180edfebc496
goodwid <goodwid@gmail.com>
2016-04-26 13:24:30 -0700

    Successfully retrieved data!  Next step: filtering data.

--- scripts/repo.js
+++ scripts/repo.js
@@ -6,19 +6,23 @@
   repos.requestRepos = function(callback) {
     // DONE: How would you like to fetch your repos? Don't forget to call the callback. Hint: What did you learn on Day 6? Use the method that lets you send a HEAD
 
-    var Url = 'https://api.github.com/user/repos';
+    var url = 'https://api.github.com/user/repos';
+    var authString = 'token '+ githubToken;
     var jqXHR = $.ajax({
       url : url,
       type : 'GET',
       dataType : 'JSON',
       headers : {
-        "Authorization" : githubToken
+        "Authorization" : authString
       },
     }).done( function (data) {
       data.map( function(obj) {
         repos.all.push(obj);
       });
     });
+    if (callback) {
+      callback();
+    }
   };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute. You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
